8de7004abec0e761c7c28a39731558e39ef952f1
Alunos <aluno.etec@etecEmbu.com.br>
2022-08-29 20:26:11 -0300

    Dinowiki1

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+console.log('hello world'); //criar um comentario de linha
+/*
+criar um cometário de bloco
+assim posso fazer comentarios em varias linhas
+*/
+let n1 = 32;
+let n2 = 68;
+let result = n1 + n2
+
+console.log (n1);
+console.log (n2)
+
+console.log('o valor da soma é:' , result)
